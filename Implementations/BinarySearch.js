7d277b4ecad8befda5a949dbe6a2afab87b6aee9
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2023-02-06 18:52:16 +0530

    Create BinarySearch.js

---
+++ Implementations/BinarySearch.js
@@ -0,0 +1,27 @@
+const arrToFind = [5,10,15,20,21,22,22,22,222,222222]
+
+
+function BinarySearch(arr,element) {
+    
+    let start = 0
+    let end = arr.length - 1
+    
+    while(start <= end) {
+        let mid = Math.floor((start+end)/2)
+        
+        if(arr[mid] == element) {
+            return mid
+        }
+        
+        if(element < arr[mid]) {
+            end = mid -1
+        }
+        else {
+            start = mid + 1
+        }
+    }
+    
+    return -1
+}
+
+console.log(BinarySearch(arrToFind,222))
